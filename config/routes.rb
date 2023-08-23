a244b8460d152d174394f4da6d8bcbabdf778ad5
formicaster <140141441+formicaster@users.noreply.github.com>
2023-08-23 11:25:07 +0000

    [Add] destroy [Update] routing-resources-update,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
   get '/top' => 'homes#top'
   resources :books
-  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
